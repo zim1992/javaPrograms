88b25a38798e8796e4d10ea57d122edf3355fd8b
Zim1992 <Zim1992@github.com>
2016-06-04 12:07:19 +0200

    just made java files

---
+++ lab5/dataSorter/src/datasorter/DataSorter.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datasorter;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author corjan
+ */
+public class DataSorter {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String fileName="";
+        // TODO code application logic here
+    }
+    
+}
